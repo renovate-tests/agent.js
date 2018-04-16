4732d98c549da8195d64fea9be66a5853c66b329
Shuta Kumano <shoota.dev@gmail.com>
2018-04-16 14:21:13 +0900

    add vwo integration example

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,8 @@ module.exports = {
   entry: {
     'agent.d': path.join(__dirname, 'packages/agent/src/entrypoint/debug.ts'),
     agent: path.join(__dirname, 'packages/agent/src/entrypoint/index.ts'),
-    linker: path.join(__dirname, 'packages/linker/src/index.ts')
+    linker: path.join(__dirname, 'packages/linker/src/index.ts'),
+    'vwo-plugin': path.join(__dirname, 'packages/vwo/src/index.ts')
   },
   output: {
     path: path.join(__dirname, 'cdn'),
